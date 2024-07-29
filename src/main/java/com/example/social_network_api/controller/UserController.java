46db83626a51d9d6083ee3ce9246d7a5fefa5c76
mahiv87 <maherreraiv1987@gmail.com>
2024-07-29 17:17:29 -0600

    debugged deleteUser

--- src/main/java/com/example/social_network_api/controller/UserController.java
+++ src/main/java/com/example/social_network_api/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.social_network_api.controller;
 
+import com.example.social_network_api.model.Thought;
 import com.example.social_network_api.model.User;
 import com.example.social_network_api.repository.ThoughtRepository;
 import com.example.social_network_api.repository.UserRepository;
@@ -32,5 +33,38 @@ public class UserController {
                 .map(user -> ResponseEntity.ok().body(user))
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No user with that ID found..."));
     }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<User> updateUser(@PathVariable String userId, @RequestBody User userDetails) {
+        return userRepository.findById(userId)
+                .map(user -> {
+                    user.setUsername(userDetails.getUsername());
+                    user.setEmail(userDetails.getEmail());
+                    user.setThoughts(userDetails.getThoughts());
+                    user.setFriends(userDetails.getFriends());
+                    User updatedUser = userRepository.save(user);
+                    return ResponseEntity.ok().body(updatedUser);
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No user with that ID found..."));
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable String userId) {
+        userRepository.findById(userId)
+                .map(user -> {
+                    if (user.getThoughts() != null && !user.getThoughts().isEmpty()) {
+                        thoughtRepository.deleteAllById(user.getThoughts().stream().map(Thought::getId).toList());
+                    }
+                    userRepository.delete(user);
+                    return ResponseEntity.ok().build();
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No user with that ID found..."));
+    }
+
 
 }
