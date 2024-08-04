b93df7bc9039505c88476e34cce09424edbc44ae
mahiv87 <maherreraiv1987@gmail.com>
2024-08-04 08:36:44 -0600

    getSingleThought implementation

--- src/main/java/com/example/social_network_api/controller/ThoughtController.java
+++ src/main/java/com/example/social_network_api/controller/ThoughtController.java
@@ -5,9 +5,13 @@ import com.example.social_network_api.repository.ReactionRepository;
 import com.example.social_network_api.repository.ThoughtRepository;
 import com.example.social_network_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -27,6 +31,13 @@ public class ThoughtController {
     @GetMapping
     public List<Thought> getThoughts() {
         return thoughtRepository.findAll();
+    }
+
+    @GetMapping("/{thoughtId}")
+    public ResponseEntity<Thought> getSingleThought(@PathVariable String thoughtId) {
+       return thoughtRepository.findById(thoughtId)
+                .map(thought -> ResponseEntity.ok().body(thought))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No thought with that Id found..."));
     }
 
 
